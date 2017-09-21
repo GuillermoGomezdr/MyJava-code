93377e32ba8d38a033da34359a67bbb2ef0bb31a
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-09-21 19:43:07 +0200

    Subiendo aún más ejercicios.

---
+++ 2Curso/src/Tema0_Fibonacci.java
@@ -0,0 +1,19 @@
+
+public class Tema0_Fibonacci {
+
+	static int fibon(int n){	
+		if (n == 0 || n == 1)
+			return 1;
+		return fibon(n-1) + fibon(n-2);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int resultado = fibon(8);
+		
+		System.out.println("El resultado es: "+resultado);
+
+	}
+
+}
